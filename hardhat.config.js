ed1194395a7e6507ca050e430b221a93a7648124
Evangel90 <oladipoevangel90@gmai.com>
2024-10-02 01:37:54 +0100

    ready to deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    url: process.env.API_URL,
+    accounts:[process.env.KEY]
+  }
 };
